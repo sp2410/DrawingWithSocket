fb27c6f2002196a56ce928fc0d7d2ad011a95b02
sp2410 <saprakas@syr.edu>
2017-03-10 23:46:23 -0500

    heroku port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 //listen on port for requests
-var server = app.listen(3000);
+var server = app.listen(process.env.PORT || 3000);
 
 app.use(express.static('public'));
 
